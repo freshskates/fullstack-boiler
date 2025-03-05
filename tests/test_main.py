aeddeae6f205cd8351442878c96e6df30b78a365
freshskates <freshskates@proton.me>
2025-03-04 16:53:47 -0800

    add gh workflow

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,4 +6,4 @@ def test_read_main(test_client):
 def test_get_output(test_client):
     response = test_client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "Worldd"}
+    assert response.json() == {"Hello": "World"}
